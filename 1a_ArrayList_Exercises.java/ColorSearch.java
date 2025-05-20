e7fe84a58f60d7f3ba2c921bb7efba93d1da433f
Manoharibr114 <manoharibrmanu@gmail.com>
2025-05-20 19:09:56 +0530

    Add files via upload

---
+++ 1a_ArrayList_Exercises.java/ColorSearch.java
@@ -0,0 +1,30 @@
+/* Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not  */
+
+
+
+package prog1;
+import java.util.*;
+public class ColorSearch {
+	
+	    public static void main(String[] args) {
+	        // Creating an ArrayList to store color names
+	        ArrayList<String> colors = new ArrayList<>();
+
+	        // Adding elements to the ArrayList
+	        colors.add("Red");
+	        colors.add("Green");
+	        colors.add("Blue");
+	        colors.add("Yellow");
+
+	        // Displaying all colors
+	        System.out.println("Colors: " + colors);
+
+	        // Searching for "Red" in the list
+	        if (colors.contains("Red")) {
+	            System.out.println("Color 'Red' is available.");
+	        } else {
+	            System.out.println("Color 'Red' is not available.");
+	        }
+	    }
+	}
