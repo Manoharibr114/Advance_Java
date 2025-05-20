22c194c34b02a4aa844845e67741f42aba0e3c95
Manoharibr114 <manoharibrmanu@gmail.com>
2025-05-20 19:22:42 +0530

    Add files via upload

---
+++ 1a_ArrayList_Exercises.java/ColorSorter.java
@@ -0,0 +1,32 @@
+/*Write a java program for getting different colors through ArrayList interface and sort them
+using Collections.sort( ArrayListObj)*/
+
+
+package prog1;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ColorSorter {
+	
+	    public static void main(String[] args) {
+	        // Creating an ArrayList of colors
+	        ArrayList<String> colors = new ArrayList<>();
+
+	        // Adding unsorted colors
+	        colors.add("Yellow");
+	        colors.add("Red");
+	        colors.add("Green");
+	        colors.add("Blue");
+
+	        System.out.println("Before Sorting: " + colors);
+
+	        // Sorting the ArrayList alphabetically
+	        Collections.sort(colors);
+
+	        // Display sorted list
+	        System.out.println("After Sorting: " + colors);
+	    }
+	}
+
+	
+
