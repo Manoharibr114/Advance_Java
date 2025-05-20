188d453f3c77afe9674daf8588d59bd5f61134d5
Manoharibr114 <manoharibrmanu@gmail.com>
2025-05-20 19:11:04 +0530

    ArrayList1.java

--- 1a_ArrayList_Exercises.java/ArrayList1.java
+++ 1a_ArrayList_Exercises.java/ArrayList1.java
@@ -1,7 +1,5 @@
-/* Write a java program for getting different colors through ArrayList interface and search whether
+/*b1. Write a java program for getting different colors through ArrayList interface and search whether
 the color "Red" is available or not  */
-
-
 
 package prog1;
 import java.util.*;
